50d70ddba60a392b5be3825bb4df5b6274c55e7f
Didarul Alam Sourov <da.sourov.11@gmail.com>
2024-04-12 23:39:31 +0200

    v1

---
+++ my-project/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+        <div>
+          <h1 className='bg-red-500 text-white p-5'>Hey there</h1>
+        </div>
+    </>
+  )
+}
+
+export default App
